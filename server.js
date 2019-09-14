6e97e97f548c3f02f688b87175369f1a371f494a
Andy Nguyen <apnguyen11@yahoo.com>
2019-09-14 12:47:33 -0500

    learned how to implements routes to external moduels to load on website via node

--- server.js
+++ server.js
@@ -19,33 +19,64 @@ var tweets = require('./abstractions/tweets.js');
 app.use(express.static(__dirname + '/public'));
 
 // GET /api/albums
-
+function handleAlbums(request, response, nextFn){
+    response.json(albums);
+}
+app.get('/api/albums', handleAlbums)
 
 // GET /api/circles
-
+function handleCircles(req, res, nextFn) {
+    res.json(circles)
+ }
+ app.get('/api/circles', handleCircles)
 
 // GET /api/movies
-
+app.get('/api/movies', function(request, response, nextFn){
+    response.json(movies)
+})
 
 // GET /api/nametags
-
+function handleNameTags(request, response, nextFn){
+    response.json(nameTags);
+}
+app.get('/api/nametags', handleNameTags)
 
 // GET /api/poker
 
+app.get('/api/poker', function(req, res, nextFn){
+    res.json(poker)
+})
 
 // GET /api/rectangle
-
+function handleRectangle(req, res, nextFn) {
+    res.json(rectangle)
+ }
+ app.get('/api/rectangle', handleRectangle)
 
 // GET /api/restaurants
+function handleRestaurants(req, res, nextFn){
+    res.json(restaurants)
+}
 
+app.get('/api/restaurants', handleRestaurants)
 
 // GET /api/students
 
+app.get('/api/students', function(req, res, nextFn){
+    res.json(students)
+})
 
 // GET /api/surveys
 
+app.get('/api/surveys', function(req, res, nextFn){
+    res.json(surveys)
+})
+
 
 // GET /api/tweets
+app.get('/api/tweets', function(req, res, nextFn){
+    res.json(tweets)
+})
 
 
 app.listen(3000, function(){
